dcaf6df7a151951c868801d9c25c483f3985b1ba
Manirul Islam Tanim <manirul41@gmail.com>
2021-09-29 20:39:46 +0600

    init react with tailwindcss boilerplate :)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto px-4">
+      <div className="flex flex-col items-center justify-center h-screen">
+        <div className="bg-yellow-400 p-2 font-bold text-4xl m-2">Hi!</div>
+        <div className="bg-yellow-400 p-2 font-bold text-4xl">React | Tailwindcss | ESLint | Prettier</div>
+      </div>
     </div>
   );
 }
